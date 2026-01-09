0b5380cb2176385fdb69b2c081986c147389c481
anthocab2 <anthonycaban1010@gmail.com>
2026-01-09 14:56:46 -0400

    2. Selection sort: implement selection_sort function and add Big O notations

--- sort.h
+++ sort.h
@@ -1,15 +1,9 @@
 #ifndef SORT_H
 #define SORT_H
-
 #include <stddef.h>
+#include <stdio.h>
 
-/**
- * struct listint_s - Doubly linked list node
- *
- * @n: Integer stored
- * @prev: Previous node
- * @next: Next node
- */
+/* Data structure for doubly linked list */
 typedef struct listint_s
 {
 const int n;
@@ -17,9 +11,11 @@ struct listint_s *prev;
 struct listint_s *next;
 } listint_t;
 
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
+/* Function prototypes */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
-#endif
+#endif /* SORT_H */
